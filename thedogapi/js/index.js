c425ff98aea0873d154ac45f7be62c24de7c75f4
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-24 07:24:58 -0800

    third commit

--- thedogapi/js/index.js
+++ thedogapi/js/index.js
@@ -4,11 +4,27 @@
 const loadDog =() =>{
     fetch('https://api.thedogapi.com/v1/breeds')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayDog(data))
 }
 
 
 
-const displayDog(){
-    
+const displayDog=(dog)=>{
+    // console.log(data);
+  const main= document.getElementById('main');
+//   const firstTen= dog.slice(0,10);
+  const all= dog;
+//   console.log(firstTen)
+
+for(const  dog of all){
+    const div= document.createElement('div');
+    //  console.log(dog);
+    div.className="col-lg-6"
+    div.innerHTML=`<h2>${dog.name}</h2>
+    <img width="400px" height="250px" src="${dog.image.url}"  alt="...">
+    <p> Dog Weight : <span>${dog.weight.imperial} </span> ,  <span>${dog.weight.metric} </span></p>`
+    main.appendChild(div);
+
+}
+
 }

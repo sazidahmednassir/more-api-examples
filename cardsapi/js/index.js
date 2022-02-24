940d024b5e4ecc69278ea344548ab19341940c38
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-24 09:23:57 -0800

    card last commit

--- cardsapi/js/index.js
+++ cardsapi/js/index.js
@@ -40,11 +40,39 @@ const displayCards= cards =>{
   <div class="card-body">
     <h5 class="card-title">${card.suit}</h5>
     <p class="card-text">${card.code}</p>
-    <a href="#" class="btn btn-primary">See Details</a>
+    <button onclick="cardDeatils('${card.code}')" class="btn btn-primary"> See Details</buuton>
+  
   </div>
 </div>
         `
         main.appendChild(div);
     }
   
+}
+
+//<a href="#" class="btn btn-primary">See Details</a>
+
+const cardDeatils= (code)=>{
+//   console.log(code)
+fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
+.then(res => res.json())
+.then (data =>{
+    const allcards= data.cards;
+    const singleCard= allcards.find(card=>card.code === code)
+    const div=document.createElement('div');
+    main.innerHTML='';
+    div.classList.add('my-3')
+    div.innerHTML= `<div class="card " style="width: 18rem;">
+    <img src="${singleCard.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${singleCard.suit}</h5>
+      <p class="card-text">${singleCard.code}</p>
+      <p class="card-text">${singleCard.value}</p>
+  
+    
+    </div>
+    `
+    main.appendChild(div)
+})
+
 }

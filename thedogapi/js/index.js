4d177903747b40d231d508c15f5b198189db5acc
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-24 06:33:49 -0800

    seconf commit

---
+++ thedogapi/js/index.js
@@ -0,0 +1,14 @@
+
+
+
+const loadDog =() =>{
+    fetch('https://api.thedogapi.com/v1/breeds')
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+
+
+const displayDog(){
+    
+}

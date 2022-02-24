25c25b724803ee012f66699749f4d9e49cc45704
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-24 08:57:13 -0800

    seven commit

--- cardsapi/js/index.js
+++ cardsapi/js/index.js
@@ -1,5 +1,5 @@
 
-
+const main=document.getElementById('main');
 
 const loadCards = () =>{
     const error=document.getElementById('error');
@@ -22,11 +22,29 @@ const loadCards = () =>{
         error.className="d-none"
         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputText}`)
         .then(res => res.json())
-        .then (data => displayCards(data))
+        .then (data => displayCards(data.cards))
     }
 }
 
 
 const displayCards= cards =>{
-    console.log(cards)
+    // console.log(cards)
+    for(const card of cards ){
+        // console.log(card)
+        const div=document.createElement('div');
+        div.classList.add("col-lg-4")
+        div.classList.add('my-3')
+        div.innerHTML=`
+        <div class="card " style="width: 18rem;">
+  <img src="${card.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${card.suit}</h5>
+    <p class="card-text">${card.code}</p>
+    <a href="#" class="btn btn-primary">See Details</a>
+  </div>
+</div>
+        `
+        main.appendChild(div);
+    }
+  
 }
